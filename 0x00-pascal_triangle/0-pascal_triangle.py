6726470c4ccb15f72e7e3f13abed440ad225b053
Younrag <younes.raguebi@usmba.ac.ma>
2024-04-26 00:33:59 +0100

    Done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import math
+fact = math.factorial
+
+
+def pascal_triangle(n):
+    """
+    create triangle
+    """
+    i = 0
+    T = []
+    if n <= 0:
+        return T
+    while i < n:
+        row = [int(fact(i) / (fact(j) * fact(i - j))) for j in range(0, i + 1)]
+        T.append(row)
+        i = i + 1
+    return T
