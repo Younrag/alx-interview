3c15337074c2723a58de25384ab3b4e0a636270d
Younrag <younes.raguebi@usmba.ac.ma>
2024-05-09 23:27:29 +0100

    Done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module for 0-minoperations"""
+
+
+def minOperations(n):
+    """
+    minOperations
+    Gets fewest # of operations needed
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops
