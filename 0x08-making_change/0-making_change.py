f484976e9b4467086763f7c806c4dba064a00c97
Younrag <younes.raguebi@usmba.ac.ma>
2024-06-21 00:42:19 +0100

    Done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,30 +3,24 @@
 Change comes from within
 """
 
+
 def makeChange(coins, total):
-  """
-  Calculates the minimum number of coins needed to make a certain amount of money.
-  """
-  if total <= 0:
+    """ Calculates the minimum number of coins needed
+    to make a certain amount of money.
+    """
+    if total <= 0:
         return 0
-  
-  remainder = total
-
-  coins_needed = 0
-
-  coin_index = 0
-
-  sorted_coins_list = sorted(coins, reverse=True)
-
-  list_len = len(coins)
-
-  while remainder > 0:
-      if coin_index >= list_len:
-          return -1
-
-      if remainder - sorted_coins_list[coin_index] >= 0:
-          remainder -= sorted_coins_list[coin_index]
-          coins_needed += 1
-      else:
-          coin_index += 1
-  return coins_needed
+    remainder = total
+    coins_needed = 0
+    coin_index = 0
+    sorted_coins_list = sorted(coins, reverse=True)
+    list_len = len(coins)
+    while remainder > 0:
+        if coin_index >= list_len:
+            return -1
+        if remainder - sorted_coins_list[coin_index] >= 0:
+            remainder -= sorted_coins_list[coin_index]
+            coins_needed += 1
+        else:
+            coin_index += 1
+    return coins_needed
