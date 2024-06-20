84cbe21a0cd98aa0c64a9483a7f2c9391b470b88
Younrag <younes.raguebi@usmba.ac.ma>
2024-06-21 00:24:59 +0100

    Done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,19 +11,23 @@ def make_change(coins, total):
         return 0
 
     remainder = total
+
     coins_needed = 0
 
-    sorted_coins_list = sorted(coins, reverse=True)
-    list_len = len(coins)
     coin_index = 0
 
-    while remainder > 0: 
+    sorted_coins_list = sorted(coins, reverse=True)
+
+    list_len = len(coins)
+
+    while remainder > 0:
+
         if coin_index >= list_len:
             return -1
+
         if remainder - sorted_coins_list[coin_index] >= 0:
             remainder -= sorted_coins_list[coin_index]
             coins_needed += 1
         else:
             coin_index += 1
-
     return coins_needed
